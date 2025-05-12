0f3dd3fc09e3a1f88e32932cee4950ce284f28f1
MorettiAndrea <andrea.moretti1704@gmail.com>
2025-05-12 17:37:11 +0200

    finita la logica sposto l'array su nuovo file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const localHost = "http://localhost:3000";
+const localHost = `http://localhost:${port}`;
 
 app.use(express.static("public"));
 
@@ -9,43 +9,48 @@ app.get("/", (req, res) => {
   res.json({ message: "Benvenuti sul mio blog" });
 });
 
-app.get("/I Miei Post", (req, res) => {
+app.get("/Bacheca", (req, res) => {
   const iMieiPost = [
     {
       id: 1,
       titolo: "Il ciambellone",
       contenuto: "Ricetta del ciambellone",
-      immagine: "ciambellone.jpeg",
+      immagine: `${localHost}/img/ciambellone.jpeg`,
       tags: ["colazione", "ricette"],
     },
     {
       id: 2,
       titolo: "Cracker Barbabietola",
       contenuto: "Ricetta del Cracker Barbabietola ",
-      immagine: "barbabietola.jpeg",
+      immagine: `${localHost}/img/cracker_barbabietola.jpeg`,
       tags: ["snack", "ricette"],
     },
     {
       id: 3,
       titolo: "Pane fritto dolce",
       contenuto: "Ricetta del Pane fritto dolce",
-      immagine: "fritto_dolce.jpeg",
+      immagine: `${localHost}/img/pane_fritto_dolce.jpeg`,
       tags: ["pane fritto", "ricette"],
     },
     {
       id: 4,
       titolo: "Pasta barbabietola",
       contenuto: "Ricetta della Pasta barbabietola",
-      immagine: "barbabietola.jpeg",
+      immagine: `${localHost}/img/pasta_barbabietola.jpeg`,
       tags: ["pasta", "ricette"],
     },
     {
       id: 5,
       titolo: "Torta paesana",
       contenuto: "Ricetta della Torta paesana",
-      immagine: "torta_paesana.jpeg",
+      immagine: `${localHost}/img/torta_paesana.jpeg`,
       tags: ["torte", "ricette"],
     },
   ];
+
   res.json(iMieiPost);
+});
+
+app.listen(port, () => {
+  console.log(`Server in ascolto su ${localHost}`);
 });
